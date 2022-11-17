6c38bb750bdd00144c5fc61d0e2271cb1a5a0590
Anthony Edmonds <anthony.edmonds@live.co.uk>
2022-11-17 09:28:08 +0000

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# laravel-mysqlite
-Generate MySQL and Sqlite3 expressions on the fly based on the active database
+# Laravel MySqlite
+
+Generate MySQL and SQLite3 expressions on the fly based on the active database.
+
+Designed primarily for situations where an SQLite3 database is used for testing, but MySQL is used for production.
+
+Calling the `MySqlite::`  helper will provide the correct syntax for the default database connection.
+
+## Installation
+
+1. Install via Composer: `composer require anthonyedmonds\laravel-mysqlite`
+2. Use in your code: `use AnthonyEdmonds\LaravelMySqlite\MySqlite`
+
+## Usage
+
+Laravel MySqlite provides syntax for the following MySQL/SQLite conversions:
+
+| MySqlite Method        | MySQL Syntax    | SQLite Syntax | Notes |
+| ---------------------- | --------------- | ------------- | ----- |
+| MySqlite::cast()       | `CAST()`        | `CAST()`      | Must be a value from `MySqlite::CASTS_MYSQL` |
+| MySqlite::concat()     | `CONCAT()`      | `\|\|`          | Pass literal strings with quotation marks, such as `'"String"'` |
+| MySqlite::day()        | `DAY()`         | `STRFTIME()`  | |
+| MySqlite::dateFormat() | `DATE_FORMAT()` | `STRFTIME()`  | Use date formats supported by both MySQL and SQLite |
+| MySqlite::hour()       | `HOUR()`        | `STRFTIME()`  | |
+| MySqlite::month()      | `MONTH()`       | `STRFTIME()`  | |
+| MySqlite::year()       | `YEAR()`        | `STRFTIME()`  | |
+
+The helper returns a `DB::raw` expression, so you may use it directly inside queries:
+
+```php
+DB::table('users')
+    ->select([
+        MySqlite::concat(['users.first_name', '" "', 'users.last_name'], 'name'),
+        MySqlite::dateFormat('users.created_at', '%d/%m/%Y', 'formatted_date'),
+        MySqlite::day('AVG(users.updated_at)', 'aggregated_day'),
+    ])
+    ->groupBy([
+        MySqlite::year('users.updated_at')
+    ])
+    ->get();
+```
+
+### Columns
+The column/columns value of a method accepts a string/array of:
+
+* A column name `'users.first_name'`
+* An aggregate method `'COUNT(users.id)'`
+* A string literal in quotation marks `'"Literally this"'`
+* Any other valid SQL expression
+
+### Aliasing / As
+
+The optional `$as` parameter adds a column alias when set:
+
+`MySqlite::hour('users.created_at', 'potato') === YEAR(users.created_at) AS potato`
+
+## Roadmap
+
+There are more syntax differences between MySQL and SQLite that can be compensated for with minor syntax changes, which will be added as they are encountered.
+
+If you require a particular method conversion, raise an issue.
---
+++ src/MySqlite.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace AnthonyEdmonds\LaravelMySqlite;
+
+use ErrorException;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Tired of having to deal with the differences between SQLite and MySql?
+ * So am I. Call this helper to help you out when you need interoperability!
+ *
+ * Use a "quotation mark" when passing literal strings
+ *
+ * @author Anthony Edmonds
+ * @link https://github.com/AnthonyEdmonds/laravel-mysqlite
+ */
+class MySqlite
+{
+    /* Casts ================================= */
+    const DATE = 'DATE';
+    const DATETIME = 'DATETIME';
+    const DECIMAL = 'DECIMAL';
+    const TIME = 'TIME';
+    const CHAR = 'CHAR';
+    const NCHAR = 'NCHAR';
+    const SIGNED = 'SIGNED';
+    const UNSIGNED = 'UNSIGNED';
+    const BINARY = 'BINARY';
+
+    const CASTS_MYSQL = [
+        self::DATE,
+        self::DATETIME,
+        self::DECIMAL,
+        self::TIME,
+        self::CHAR,
+        self::NCHAR,
+        self::SIGNED,
+        self::UNSIGNED,
+        self::BINARY,
+    ];
+
+    const CASTS_SQLITE = [
+        self::DATE => 'NUMERIC',
+        self::DATETIME => 'NUMERIC',
+        self::DECIMAL => 'NUMERIC',
+        self::TIME => 'NUMERIC',
+        self::CHAR => 'TEXT',
+        self::NCHAR => 'TEXT',
+        self::SIGNED => 'INTEGER',
+        self::UNSIGNED => 'INTEGER',
+        self::BINARY => 'TEXT',
+    ];
+
+    public static function cast(string $column, string $type, string $as = null): Expression
+    {
+        if (in_array($type, self::CASTS_MYSQL) === false) {
+            throw new ErrorException("$type is not a recognised MySQL cast type");
+        }
+
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? "CAST($column AS " . self::CASTS_SQLITE[$type] . ")" . self::as($as)
+                : "CAST($column AS $type)" . self::as($as)
+        );
+    }
+
+    /* Dates ================================= */
+
+    public static function dateFormat(string $column, string $format, string $as = null): Expression
+    {
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? "STRFTIME('$format', $column)" . self::as($as)
+                : "DATE_FORMAT($column, '$format')" . self::as($as)
+        );
+    }
+
+    public static function year(string $column, string $as = null): Expression
+    {
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? "STRFTIME('%Y', $column)" . self::as($as)
+                : "YEAR($column)" . self::as($as)
+        );
+    }
+
+    public static function month(string $column, string $as = null): Expression
+    {
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? "STRFTIME('%m', $column)" . self::as($as)
+                : "MONTH($column)" . self::as($as)
+        );
+    }
+
+    public static function day(string $column, string $as = null): Expression
+    {
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? "STRFTIME('%d', $column)" . self::as($as)
+                : "DAY($column)" . self::as($as)
+        );
+    }
+
+    public static function hour(string $column, string $as = null): Expression
+    {
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? "STRFTIME('%H', $column)" . self::as($as)
+                : "HOUR($column)" . self::as($as)
+        );
+    }
+
+    /* Strings =============================== */
+    public static function concat(array $columns, string $as = null): Expression
+    {
+        return DB::raw(
+            DB::getDefaultConnection() === 'sqlite'
+                ? implode(' || ', $columns) . self::as($as)
+                : 'CONCAT(' . implode(',', $columns) . ')' . self::as($as)
+        );
+    }
+
+    /* Utilities ============================= */
+    protected static function as(string|null $as = null): string
+    {
+        return $as !== null ? " AS $as" : '';
+    }
+}
